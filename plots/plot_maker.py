f949bb7309745a32c13f7a09271d8bc052acf489
Sarah Zwiep <szwiep@sfu.ca>
2021-06-14 13:41:37 -0700

    Enable all plotting, .gitingore subdirectories

--- plots/plot_maker.py
+++ plots/plot_maker.py
@@ -14,10 +14,10 @@ iteration_range = [10, 30]
 y_limit = 10
 
 with shelve.open(fp_in, 'r') as shelf:
-    # for iter in range(iteration_range[0], iteration_range[1]+1):
-    #     plotting.stream(iter, np.array(shelf["bed"]), np.array(shelf[str(iter)][0]), 
-    #                     shelf["param"]["x_max"], y_limit, np.array(shelf[str(iter)][1]), fp_out)
-    # plotting.flux_info(np.array(shelf["flux"]), shelf["param"]["n_iterations"], fp_out)
-    # plotting.flux_info2(np.array(shelf["flux"]), np.array(shelf["avg_age"]), shelf["param"]["n_iterations"], fp_out)
+    for iter in range(iteration_range[0], iteration_range[1]+1):
+        plotting.stream(iter, np.array(shelf["bed"]), np.array(shelf[str(iter)][0]), 
+                        shelf["param"]["x_max"], y_limit, np.array(shelf[str(iter)][1]), fp_out)
+    plotting.flux_info(np.array(shelf["flux"]), shelf["param"]["n_iterations"], fp_out)
+    plotting.flux_info2(np.array(shelf["flux"]), np.array(shelf["avg_age"]), shelf["param"]["n_iterations"], fp_out)
     plotting.flux_info3(np.array(shelf["flux"]), np.array(shelf["avg_age"]), np.array(shelf["age_range"]), 
                         shelf["param"]["n_iterations"], fp_out)

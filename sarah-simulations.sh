756d2a202984be3198b1f9676e9b7be1fd09eddd
Sarah Zwiep <szwiep@sfu.ca>
2021-07-19 14:08:48 -0700

    Bash scripts for experimental runs

---
+++ sarah-simulations.sh
@@ -0,0 +1,4 @@
+python3 model/parallel.py 2 model/parameters/param-sim11.yaml model/parameters/param-sim12.yaml
+python3 model/parallel.py 2 model/parameters/param-sim13.yaml model/parameters/param-sim14.yaml
+python3 model/parallel.py 2 model/parameters/param-sim15.yaml model/parameters/param-sim16.yaml
+python3 model/parallel.py 2 model/parameters/param-sim17.yaml model/parameters/param-sim18.yaml

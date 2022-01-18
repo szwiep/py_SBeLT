cf1ca87dd37ec3afa5f04fd8831251f2e3a2e3a9
szwiep <szwiep@sfu.ca>
2022-01-17 21:11:02 -0800

    Remove experiment shell files from repo

--- sarah-simulations.sh
+++
@@ -1,6 +0,0 @@
-python3 model/parallel.py 2 model/parameters/param-sim13.yaml model/parameters/param-sim14.yaml
-python3 model/parallel.py 2 model/parameters/param-sim15.yaml model/parameters/param-sim16.yaml
-python3 model/parallel.py 2 model/parameters/param-sim17.yaml model/parameters/param-sim18.yaml
-python3 model/parallel.py 2 model/parameters/param-sim19.yaml model/parameters/param-sim20.yaml
-python3 model/parallel.py 2 model/parameters/param-sim21.yaml model/parameters/param-sim22.yaml
-python3 model/parallel.py 2 model/parameters/param-sim23.yaml model/parameters/param-sim24.yaml
